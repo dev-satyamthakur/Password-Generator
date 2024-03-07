a9bc20df54d94dab8d34867323e3c992c788b45c
Satyam Thakur <sat.thakur20@gmail.com>
2024-03-07 15:55:07 +0530

    Implementing copy password button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +8,8 @@ function App() {
   const [numbersAllowed, setNumbersAllowed] = useState(false);
   const [charsAllowed, setCharsAllowed] = useState(false);
   const [password, setPassword] = useState('');
+
+  const passwordRef = useRef(null);
 
   const generatePassword = useCallback(() => {
     let pass = "";
@@ -33,13 +35,18 @@ function App() {
     generatePassword();
   }, [length, numbersAllowed, charsAllowed])
 
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+  }
+
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
         <h1 className='text-3xl font-bold mb-2 text-center text-white'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly/>
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+          <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly ref={passwordRef}/>
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={copyPasswordToClipboard}>Copy</button>
         </div>
 
         <div className='flex text-sm gap-x-2'>
